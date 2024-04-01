ce715dd7ff8c4a7211ace17eb176c240d36f1189
unknown <you@example.com>
2024-04-01 14:15:54 -0300

    first commit

---
+++ screenmatch/screenmatch/src/main/java/br/com/garitano/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,34 @@
+package br.com.garitano.screenmatch;
+
+import br.com.garitano.screenmatch.model.DadosSerie;
+import br.com.garitano.screenmatch.service.ConsumoApi;
+import br.com.garitano.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6cb59bc");
+
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+
+
+
+	}
+}
---
+++ screenmatch/screenmatch/src/main/java/br/com/garitano/screenmatch/service/IConverteDados.java
@@ -0,0 +1,6 @@
+package br.com.garitano.screenmatch.service;
+
+public interface IConverteDados {
+    <T> T obterDados(String json, Class<T> classe);
+
+}
